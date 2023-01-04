b475cf199ceab11aff6faa5660b82fdd44a843d5
muradorucov <orucowmurad@gmail.com>
2023-01-04 21:53:34 +0400

    feat: scroll to top btn added

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,3 +62,9 @@ window.addEventListener('scroll', () => {
     }
 })
 
+
+//scroll to top
+// Get the element
+let topBtn = document.querySelector(".top-btn");
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
